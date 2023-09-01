c8ad11758d28322b351ba5f4bb60a9ae5e865bd6
teoslivka@gmail.com <teoslivka@gmail.com>
2023-09-01 16:23:45 -0400

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState<String | null>(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/data').then(response => {
+      setData(response.data.message);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Message from Flask Backend</h1>
+      {data ? <div>{data}</div> : <div>Loading...</div>}
     </div>
   );
 }
